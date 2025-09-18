50f707e59cbdf56baf1b499894735388eefd0fda
Sheri Lu <sherisherilu@gmail.com>
2025-09-18 12:42:31 -0700

    Add content for step1 form.

--- src/components/Forms/Step1.jsx
+++ src/components/Forms/Step1.jsx
@@ -5,8 +5,8 @@ const textInputLabels = ['Name', 'Email Address', 'Phone Number'];
 
 const Step1 = () => {
   return (
-    <div className='bg-blue-200 flex flex-col h-full w-full justify-start items-center mt-10'>
-      <TextInput label='Name' />
+    <div className='flex flex-col h-full w-full justify-start items-start mt-10 gap-5'>
+      {textInputLabels.map((label) => <TextInput label={label} />)}
     </div>
   )
 }
--- src/components/Input/TextInput.jsx
+++ src/components/Input/TextInput.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 const TextInput = ({ label }) => {
   return (
-    <div className=''>
-      <div>{label}</div>
-      <input />
+    <div className='flex flex-col w-full'>
+      <div className='flex font-bold text-primary-blue-950 mb-2'>{label}</div>
+      <input className='flex w-full border-[1px] border-neutral-grey-500 rounded-md outline-primary-purple-600 p-2' />
     </div>
   )
 }
